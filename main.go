6889122422a502d16f066955f7277eea260ca11d
hybfkuf <fredjoseph863@gmail.com>
2022-05-14 19:29:34 +0800

    gitignore

--- main.go
+++ main.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"crypto/rsa"
+	"fmt"
+
+	grsa "github.com/forbearing/goutils/pkg/rsa"
+)
+
 func main() {
+	var (
+		privateKey *rsa.PrivateKey
+		publicKey  *rsa.PublicKey
+		err        error
+	)
+
+	privateKey, err = grsa.ReadRSAPKCS1PrivateKey("./private.pem.pkcs1")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("private key(pkcs1):", privateKey)
+
+	privateKey, err = grsa.ReadRSAPKCS8PrivateKey("./private.pem.pkcs8")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("private key(pkcs8):", privateKey)
+
+	privateKey, err = grsa.ReadRSAPrivateKey("./private.pem.pkcs1")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("private key(pkcs1):", privateKey)
+
+	privateKey, err = grsa.ReadRSAPrivateKey("./private.pem.pkcs8")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("private key(pkcs8):", privateKey)
+
+	publicKey, err = grsa.ReadRSAPublicKey("./public.pem")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("public key(pkcs8):", publicKey)
 }
